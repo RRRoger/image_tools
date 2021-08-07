08b220fd7e76a0e0c64906454e1e8ff1db1ffeb6
rrroger <chenpeng.0418@163.com>
2021-08-07 17:27:39 +0800

    [UPD] write exif in new image

--- batch_insert_exif/run.py
+++ batch_insert_exif/run.py
@@ -21,9 +21,11 @@ import exifread
 import os
 import click
 import piexif
+import pickle
+import copy
 
 FONT = "SourceHanSansCN-Normal.otf"                # 思源字体
-QUALITY = 30                                       # 导出图片质量
+QUALITY = 50                                       # 导出图片质量
 FONT_SIZE = 80                                     # 字体大小
 FONT_DEFAULT_COLOR = (0, 0, 0)                     # 字体颜色
 START_POSITION = (50, 50)                          # 字体起始位置
@@ -39,8 +41,7 @@ SHOW_TEXT = u"""相机: %(camera)s
 镜头: %(lens)s
 光圈: %(aperture)s
 快门: %(shutterspeed)s
-iso: %(iso)s
-"""
+iso: %(iso)s"""
 
 def format_aperture(val):
     """
@@ -115,9 +116,9 @@ def judge_font_color(img):
         return "Black"
 
 
-def do_write(source, dest, font_name=None, font_size=40, quality=50, set_exit=False):
+def do_write(source, dest, font_name=None, font_size=40, quality=50):
     
-    print(f"********** Insert EXIF Info: {dest} ************")
+    print(f"[Writing]: {dest}")
 
     with open(source, 'rb') as f:
         tags = exifread.process_file(f)
@@ -126,6 +127,9 @@ def do_write(source, dest, font_name=None, font_size=40, quality=50, set_exit=Fa
     show_text = SHOW_TEXT % exif_info
 
     image = Image.open(source)
+
+    # TODO: keep exif info from src image
+    # exif = image.getexif()
     color = judge_font_color(image)
     draw = ImageDraw.Draw(image)
 
@@ -142,43 +146,12 @@ def do_write(source, dest, font_name=None, font_size=40, quality=50, set_exit=Fa
         font_color = COLOR_WHITE
 
     draw.text(START_POSITION, show_text, font_color, font=font)
-    image.save(dest, format='JPEG', subsampling=0, quality=100)
-
-    if set_exit:
-        zeroth_ifd = {
-                    piexif.ImageIFD.Make: u"Canon",
-                    piexif.ImageIFD.XResolution: (96, 1),
-                    piexif.ImageIFD.YResolution: (96, 1),
-                    piexif.ImageIFD.Software: u"piexif"
-                    }
-        exif_ifd = {
-                    piexif.ExifIFD.DateTimeOriginal: u"2099:09:29 10:10:10",
-                    piexif.ExifIFD.LensMake: u"LensMake",
-                    piexif.ExifIFD.Sharpness: 65535,
-                    piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),
-                    }
-        gps_ifd = {
-                piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
-                piexif.GPSIFD.GPSAltitudeRef: 1,
-                piexif.GPSIFD.GPSDateStamp: u"1999:99:99 99:99:99",
-                }
-        first_ifd = {
-                    piexif.ImageIFD.Make: u"Canon",
-                    piexif.ImageIFD.XResolution: (40, 1),
-                    piexif.ImageIFD.YResolution: (40, 1),
-                    piexif.ImageIFD.Software: u"piexif"
-                    }
-        exif_dict = {"0th":zeroth_ifd, "Exif":exif_ifd, "GPS":gps_ifd, "1st":first_ifd}
-        exif_bytes = piexif.dump(exif_dict)
-        im = Image.open(dest)
-        im.save(dest, exif=exif_bytes)
-
-
+    image.save(dest, format='JPEG', subsampling=0, quality=quality)
 
 
 help_c = __doc__
 help_p = "修改处理的图片路径"
-help_q = f"[废弃] 图像质量, 默认: {QUALITY}"
+help_q = f"图像质量(0, 95), 默认: {QUALITY}"
 help_fs = f"字体大小, 默认: {FONT_SIZE}"
 help_font = f"选择字体, 默认: {FONT}"
 
@@ -189,27 +162,23 @@ help_font = f"选择字体, 默认: {FONT}"
 @click.option("-q", "--quality", "quality", help=help_q, type=int, required=False, default=QUALITY)
 @click.option("-fs", "--font-size", "font_size", help=help_fs, type=int, required=False, default=FONT_SIZE)
 @click.option("-font", "--font", "font", help=help_font, type=str, required=False, default=FONT)
-def main(images_dir, quality, font_size, font):
+def main(images_dir, quality, font_size, font_name):
     dir_after = os.path.join(images_dir, DIR_AFTER)
     mkdir_safe(dir_after)
 
     print("************* Start *************")
 
-    for root, dirs, files in os.walk(images_dir):
-        
-        # 忽略AFTER目录;以及其他自定义目录
-        if root in [dir_after] + BLACK_DIR_LIST: continue
+    fn_list = sorted([f for f in os.listdir(images_dir)])
+    for _i, fn in enumerate(fn_list):        
 
-        for f_name in files:
+        # 判断文件是否是图片
+        if not check_is_image(fn): continue
 
-            # 判断文件是否是图片
-            if not check_is_image(f_name): continue
+        f_path = os.path.join(images_dir, fn)  # 源文件路径
+        dest_path = os.path.join(dir_after, fn)  # 目标文件路径
 
-            f_path = os.path.join(images_dir, f_name)  # 源文件路径
-            dest_path = os.path.join(dir_after, f_name)  # 目标文件路径
-
-            # 调用处理图片函数
-            do_write(f_path, dest_path, font_name=font, font_size=font_size, quality=quality)
+        # 调用处理图片函数
+        do_write(f_path, dest_path, font_name=font_name, font_size=font_size, quality=quality)
 
     print("************* End *************")
     return "OK"
